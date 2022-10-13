56c7bb1a468853b8053af2d5d130ab18c90a2549
lisandroschneider <lisandroschneider7@gmail.com>
2022-10-13 18:54:00 -0300

    subir archivo

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=0,resultado=1;
+
+            Console.Clear();
+            
+            Console.WriteLine("Digite el numero que desea obtener su factorial");
+            n = int.Parse(Console.ReadLine());
+
+            for(int i=1;i<=n;i++)
+            {
+                resultado *= i;
+
+            }
+
+            Console.WriteLine("El resultado es: {0}",resultado);
+        
+
+        }
+    }
+}
